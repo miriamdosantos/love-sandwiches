4bd63858bb6050bcd121ac82ea07dc61ca728d58
Miriam McCay <mii.santos@yahoo.com.br>
2024-05-11 22:38:31 +0100

    Calculate stock numbers and add to stock worsheet

--- run.py
+++ run.py
@@ -103,6 +103,7 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+
 def get_last_5_entries_sales():
     """
     Collects columns of data from sales worksheet, collecting
@@ -117,6 +118,24 @@ def get_last_5_entries_sales():
         columns.append(column[-5:])
 
     return columns
+
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -126,6 +145,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 
